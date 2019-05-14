4841d283b54add997e7643e128fa0e3096d895fd
SaeedDalir <saeedkingdr@gmail.com>
2019-05-14 16:06:08 +0430

    store and update controllers and their dependencies also created edit template blade

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -4,9 +4,13 @@ namespace App\Http\Controllers\Backend;
 
 use App\Brand;
 use App\Category;
+use App\Http\Requests\Backend\ProductCreateRequest;
+use App\Http\Requests\Backend\ProductUpdateRequest;
 use App\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
@@ -28,11 +32,11 @@ class ProductController extends Controller
      */
     public function create()
     {
-        $categories = Category::with('childrenRecursive')
-            ->where('parent_id',null)
-            ->get();
+//        $categories = Category::with('childrenRecursive')
+//            ->where('parent_id',null)
+//            ->get();
         $brands = Brand::all();
-        return view('backend.products.create',compact(['categories','brands']));
+        return view('backend.products.create',compact(['brands']));
     }
 
     /**
@@ -41,9 +45,73 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function generateSKU()
     {
-        return $request->all();
+        $number = mt_rand(1000,200000);
+        if ($this->checkSKU($number)){
+            $this->generateSKU();
+        }
+        return (string)$number;
+    }
+
+    public function checkSKU($number)
+    {
+        return Product::where('sku',$number)->exists();
+    }
+
+    public function store(ProductCreateRequest $request)
+    {
+        $validator = Validator::make(
+            [
+                'categories' => $request->input('categories'),
+                'attributes' => explode(',',$request->input('attributes')[0]),
+                'photo_id' => explode(',',$request->input('photo_id')[0])
+
+            ],
+            [
+                'categories' => 'required|array',
+                'attributes' => 'required|array',
+                'attributes.*' => 'required',
+                'photo_id' => 'required|array',
+                'photo_id.*' => 'required'
+            ],
+            [
+                'categories.required'=>'لطفا دسته بندی محصول را انتخاب نمائید',
+                'attributes.*.required'=>'لطفا ویژگی محصول را انتخاب نمائید',
+                'photo_id.*.required'=>'لطفا تصویر محصول را بارگزاری نمائید'
+            ]
+        );
+        if ($validator->fails()){
+            return redirect()->back()->withErrors($validator->errors());
+        }
+        $newProduct = new Product();
+        $newProduct->title = $request->title ;
+        if ($request->slug){
+            $newProduct->slug = make_slug($request->slug);
+        }else{
+            $newProduct->slug = make_slug($request->title);
+        }
+        $newProduct->sku = $this->generateSKU();
+        $newProduct->status = $request->status;
+        $newProduct->price = $request->price;
+        $newProduct->discount_price = $request->discount_price;
+        $newProduct->desc = $request->desc;
+        $newProduct->brand_id = $request->brand;
+        $newProduct->meta_slug = $request->meta_slug;
+        $newProduct->meta_desc = $request->meta_desc;
+        $newProduct->meta_keywords = $request->meta_keywords;
+        $newProduct->user_id =1;
+        $newProduct->save();
+
+        $attributes = explode(',',$request->input('attributes')[0]);
+        $photos = explode(',',$request->input('photo_id')[0]);
+
+        $newProduct->categories()->sync($request->categories);
+        $newProduct->attributeValues()->sync($attributes);
+        $newProduct->photos()->sync($photos);
+        Session::flash('products_store','محصول با موفقیت ایجاد شد');
+
+        return redirect('/admin/products');
     }
 
     /**
@@ -65,7 +133,9 @@ class ProductController extends Controller
      */
     public function edit($id)
     {
-        //
+        $brands = Brand::all();
+        $product = Product::with(['attributeValues','brand','categories','photos'])->whereId($id)->first();
+        return view('backend.products.edit',compact(['brands','product']));
     }
 
     /**
@@ -75,9 +145,59 @@ class ProductController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(ProductUpdateRequest $request, $id)
     {
-        //
+        $validator = Validator::make(
+            [
+                'categories' => $request->input('categories'),
+                'attributes' => explode(',',$request->input('attributes')[0]),
+                'photo_id' => explode(',',$request->input('photo_id')[0])
+
+            ],
+            [
+                'categories' => 'required|array',
+                'attributes' => 'required|array',
+                'attributes.*' => 'required',
+                'photo_id' => 'required|array',
+                'photo_id.*' => 'required'
+            ],
+            [
+                'categories.required'=>'لطفا دسته بندی محصول را انتخاب نمائید',
+                'attributes.*.required'=>'لطفا ویژگی محصول را انتخاب نمائید',
+                'photo_id.*.required'=>'لطفا تصویر محصول را بارگزاری نمائید'
+            ]
+        );
+        if ($validator->fails()){
+            return redirect()->back()->withErrors($validator->errors());
+        }
+        $product = Product::findOrFail($id);
+        $product->title = $request->title ;
+        if ($request->slug){
+            $product->slug = make_slug($request->slug);
+        }else{
+            $product->slug = make_slug($request->title);
+        }
+        $product->sku = $this->generateSKU();
+        $product->status = $request->status;
+        $product->price = $request->price;
+        $product->discount_price = $request->discount_price;
+        $product->desc = $request->desc;
+        $product->brand_id = $request->brand;
+        $product->meta_slug = $request->meta_slug;
+        $product->meta_desc = $request->meta_desc;
+        $product->meta_keywords = $request->meta_keywords;
+        $product->user_id =1;
+        $product->save();
+
+        $attributes = explode(',',$request->input('attributes')[0]);
+        $photos = explode(',',$request->input('photo_id')[0]);
+
+        $product->categories()->sync($request->categories);
+        $product->attributeValues()->sync($attributes);
+        $product->photos()->sync($photos);
+        Session::flash('products_update','محصول با موفقیت بروزرسانی شد');
+
+        return redirect('/admin/products');
     }
 
     /**
@@ -88,6 +208,10 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $product = Product::findOrFail($id);
+        $product->delete();
+        Session::flash('products_delete','محصول با موفقیت حذف شد');
+
+        return redirect('/admin/products');
     }
 }
--- resources/views/backend/products/edit.blade.php
+++ resources/views/backend/products/edit.blade.php
@@ -3,61 +3,116 @@
     <link rel="stylesheet" href="{{asset('/backend/dist/css/dropzone.css')}}">
 @endsection
 @section('content')
-    <section class="content">
+    <section class="content" id="app">
         <div class="box box-info">
             <div class="box-header with-border">
-                <h3 class="box-title">ویرایش برند {{$brand->title}}</h3>
+                <h3 class="box-title">ویرایش محصول {{$product->title}}</h3>
             </div>
             <!-- /.box-header -->
             <div class="box-body">
                 <div class="row">
                     <div class="col-md-6 col-md-offset-3">
                         @include('backend.partials.form-errors')
-                        <div class="form-group center-block">
-                            <img src="{{$brand->photo->path}}" height="250px" class="center-block">
-                        </div>
-                        <form method="post" action="/admin/brands/{{$brand->id}}">
+                        <form method="post" action="/admin/products/{{$product->id}}">
                             @csrf
                             <input type="hidden" name="_method" value="PATCH">
                             <div class="form-group">
                                 <label for="title">نام</label>
-                                <input type="text" name="title" value="{{$brand->title}}" class="form-control" placeholder="نام برند را وارد کنید...">
+                                <input type="text" name="title" value="{{$product->title}}" class="form-control" placeholder="نام محصول را وارد کنید...">
+                            </div>
+                            <div class="form-group">
+                                <label for="slug">نام مستعار</label>
+                                <input type="text" name="slug" value="{{$product->slug}}" class="form-control" placeholder="نام مستعار محصول را وارد کنید...">
+                            </div>
+                            <attribute-component :brands="{{ $brands }}" :product="{{ $product }}" ></attribute-component>
+                            <div class="form-group">
+                                <label for="status">وضعیت</label>
+                                <div>
+                                    <input type="radio" name="status" value="0" @if($product->status == 0) checked  @endif><span>منتشر نشده</span>
+                                    <input type="radio" name="status" value="1" @if($product->status == 1) checked  @endif><span>منتشر شده</span>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="price">قیمت</label>
+                                <input type="number" name="price" value="{{$product->price}}" class="form-control" placeholder="قیمت محصول را وارد کنید...">
+                            </div>
+                            <div class="form-group">
+                                <label for="discount_price">قیمت ویژه</label>
+                                <input type="number" name="discount_price" value="{{$product->discount_price}}" class="form-control" placeholder="قیمت ویژه محصول را وارد کنید...">
                             </div>
                             <div class="form-group">
                                 <label for="desc">توضیحات</label>
-                                <textarea name="desc" class="form-control" placeholder="توضیحات را وارد کنید...">{{$brand->desc}}</textarea>
+                                <textarea id="description" name="desc" class="ckeditor form-control" placeholder="توضیحات محصول را وارد کنید...">{{$product->desc}}</textarea>
                             </div>
                             <div class="form-group">
-                                <label for="img">تصویر</label>
-                                <input type="hidden" name="photo_id" id="brand-photo" value="{{$brand->photo_id}}">
+                                <label for="img">گالری تصاویر</label>
+                                <input type="hidden" name="photo_id[]" id="product-photo">
                                 <div id="photo" class="dropzone"></div>
+                                <hr>
+                                <div class="row">
+                                    @foreach($product->photos as $photo)
+                                        <div class="col-sm-3" id="updated_photo_{{$photo->id}}">
+                                            <img class="img-responsive" src="{{$photo->path}}">
+                                            <button style="margin-top: 5px" type="button" class="btn btn-danger center-block" onclick="removeImages({{$photo->id}})"><i class="glyphicon glyphicon-remove"></i></button>
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="meta_slug">عنوان سئو</label>
+                                <input type="text" name="meta_slug" value="{{$product->meta_slug}}" class="form-control" placeholder="عنوان سئو محصول را وارد کنید...">
+                            </div>
+                            <div class="form-group">
+                                <label for="meta_desc">توضیحات سئو</label>
+                                <textarea name="meta_desc" class="form-control" placeholder="توضیحات سئو محصول را وارد کنید...">{{$product->meta_desc}}</textarea>
+                            </div>
+                            <div class="form-group">
+                                <label for="meta_keywords">کلمات کلیدی سئو</label>
+                                <input type="text" name="meta_keywords" value="{{$product->meta_keywords}}" class="form-control" placeholder="کلمات کلیدی سئو محصول را وارد کنید...">
                             </div>
                             <div class="form-group pull-left">
-                                <input type="submit" class="btn btn-success" value="ذخیره تغییرات">
+                                <input type="submit" onclick="productGallery()" class="btn btn-success" value="ذخیره">
                             </div>
                         </form>
                     </div>
                 </div>
-
-
             </div>
             <!-- /.box-body -->
         </div>
     </section>
 @endsection
+@section('script-vuejs')
+    <script src="{{asset('/backend/js/app.js')}}"></script>
+@endsection
 @section('scripts')
     <script src="{{asset('/backend/dist/js/dropzone.js')}}"></script>
+    <script src="{{asset('/backend/plugins/ckeditor/ckeditor.js')}}"></script>
     <script>
+        Dropzone.autoDiscover = false;
+        var photosGallery = []
+        var photos = [].concat({{$product->photos->pluck('id')}})
         var drop = new Dropzone('#photo',{
             addRemoveLinks:true,
-            maxFiles:1,
             url:"{{route('photos.upload')}}",
             sending: function(file,xhr,formData){
                 formData.append('_token','{{csrf_token()}}')
             },
             success: function (file , response) {
-                document.getElementById('brand-photo').value = response.photo_id
+                photosGallery.push(response.photo_id)
             },
         });
+        productGallery = function(){
+            document.getElementById('product-photo').value = photosGallery.concat(photos)
+        }
+        CKEDITOR.replace('description',{
+            language: 'fa',
+            customConfig: 'config.js',
+            toolbar: 'simple'
+        })
+        removeImages = function (id) {
+            var index = photos.indexOf(id)
+            photos.splice(index,1);
+            document.getElementById('updated_photo_' + id).remove();
+        }
     </script>
 @endsection
